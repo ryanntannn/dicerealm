b25bbac71cf884156eef4075b73c0481d2dd3c84
Ryan Tan <tanryan777@gmail.com>
2025-03-10 14:17:50 +0800

    chore: add android boilerplate

---
+++ android/README.md
@@ -0,0 +1,9 @@
+# Android Application
+
+This is the Android application for the project. The application is written in Java and uses the Android SDK.
+
+## Getting Started
+
+To get started with the Android application, you need to have Android Studio installed on your machine. You can download Android Studio from the [official website](https://developer.android.com/studio).
+
+To ensure that all gradle dependencies are installed, go to `File > Sync Project with Gradle Files` in Android Studio.
---
+++ android/app/src/main/java/com/example/dicerealmandroid/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.dicerealmandroid;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import dev.gustavoavila.websocketclient.WebSocketClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient webSocketClient;
+
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            // replace this with your dev device's IP address (not localhost, as localhost will point to the emulator)
+            uri = new URI("wss://better-tonye-dicerealm-f2e6ebbb.koyeb.app/room/test");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                System.out.println("onOpen");
+                webSocketClient.send("Hello, World!");
+            }
+
+            @Override
+            public void onTextReceived(String message) {
+                System.out.println("onTextReceived: " + message);
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+                System.out.println("onBinaryReceived");
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+                System.out.println("onPingReceived");
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+                System.out.println("onPongReceived");
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived(int reason, String description) {
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.addHeader("Origin", "http://developer.example.com");
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Log.d("info", "Hello!");
+
+        createWebSocketClient();
+    }
+}
