11789aeaa617dd631f39f0e63fcbc05820245302
DarrenTangent <darrentan250@gmail.com>
2025-04-04 19:07:26 +0800

    feat: Adds Skills Cooldown - Adds Cooldown timers to Skills Class - Updates CombatManager to handle cooldowns

--- core/lib/src/main/java/com/dicerealm/core/skills/Fireball.java
+++ core/lib/src/main/java/com/dicerealm/core/skills/Fireball.java
@@ -1,9 +1,10 @@
 package com.dicerealm.core.skills;
 
+import com.dicerealm.core.combat.ActionType;
 import com.dicerealm.core.entity.EntityClass;
 
 public class Fireball extends Skill {
     public Fireball() {
-        super("Fireball", "A massive ball of fire", EntityClass.WIZARD, 3,2,15);
+        super("Fireball", "A massive ball of fire", EntityClass.WIZARD, ActionType.MAGIC, 3,2,15,3);
     }
 }
--- core/lib/src/main/java/com/dicerealm/core/skills/Skill.java
+++ core/lib/src/main/java/com/dicerealm/core/skills/Skill.java
@@ -1,6 +1,8 @@
 package com.dicerealm.core.skills;
 
 import java.util.UUID;
+
+import com.dicerealm.core.combat.ActionType;
 import com.dicerealm.core.dice.MultiDice;
 import com.dicerealm.core.entity.EntityClass;
 import com.dicerealm.core.inventory.Identifiable;
@@ -14,22 +16,30 @@ public class Skill implements Identifiable {
     private String displayName;
     private String description;
     private EntityClass entityClass;
+    private ActionType actiontype;
     private MultiDice damageDice;
     private int spellSlotCost;
+    private int cooldown;
+    private int remainingCooldown;
 
-    public Skill(String name, String description, EntityClass entityClass, int spellSlotCost, int numDice, int diceSides) {
+    public Skill(String name, String description, EntityClass entityClass, ActionType actiontype, int spellSlotCost, int numDice, int diceSides, int cooldown) {
         this.id = UUID.randomUUID();
         this.displayName = name;
         this.description = description;
         this.entityClass = entityClass;
+        this.actiontype = actiontype;
         this.spellSlotCost = spellSlotCost;
         this.damageDice = new MultiDice(numDice, diceSides);
+        this.cooldown = cooldown;
+        this.remainingCooldown = 0;
     }
     public String getDisplayName() { return displayName; }
 
     public String getDescription() { return description; }
 
     public EntityClass getEntityClass(){ return entityClass; }
+
+    public ActionType getActionType() { return actiontype; }
 
     public int getSpellSlotCost() { return spellSlotCost; }
 
@@ -38,6 +48,24 @@ public class Skill implements Identifiable {
     public int rollDamage(){ return damageDice.roll(); }
 
     public UUID getId() { return id; }
+
+    public int getCooldown() { return cooldown; }
+
+    public int getRemainingCooldown() { return remainingCooldown; }
+
+    public boolean isUsable() {
+      return remainingCooldown <= 0;
+    }
+
+    public void reduceCooldown() {
+        if (remainingCooldown > 0) {
+          remainingCooldown--;
+        }
+    }
+
+    public void activateCooldown() {
+        this.remainingCooldown = cooldown;
+    }
 
     @Override
     public String toString() { return displayName + " (Spell Slot Cost: " + spellSlotCost + ", Damage: " + damageDice + ")"; }
