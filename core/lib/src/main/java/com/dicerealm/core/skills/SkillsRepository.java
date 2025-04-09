74601c022c9e8107087eae828ce3198bd1913ed2
DarrenTangent <darrentan250@gmail.com>
2025-04-10 04:05:32 +0800

    feat: Adds Default Skills to all Players

--- core/lib/src/main/java/com/dicerealm/core/skills/SkillsRepository.java
+++ core/lib/src/main/java/com/dicerealm/core/skills/SkillsRepository.java
@@ -25,12 +25,10 @@ public class SkillsRepository {
             new Skill("Shield Bash", "Bash enemy with your shield, stunning them", EntityClass.WARRIOR, ActionType.MELEE, 0, 1, 4, 2)
         ));
         warriorSkills.put(2, List.of(
-            new Skill("Cleave", "Attack multiple enemies at once", EntityClass.WARRIOR, ActionType.SKILL, 0, 1, 8, 3),
-            new Skill("Taunt", "Force enemies to attack you", EntityClass.WARRIOR, ActionType.SKILL, 0, 1, 0, 2)
+            new Skill("Cleave", "Attack multiple enemies at once", EntityClass.WARRIOR, ActionType.SKILL, 0, 2, 5, 3)
         ));
         warriorSkills.put(3, List.of(
-            new Skill("Whirlwind", "Spin attack hitting all nearby enemies", EntityClass.WARRIOR, ActionType.SKILL, 0, 2, 6, 4),
-            new Skill("Intimidating Shout", "Frighten enemies, reducing their attack", EntityClass.WARRIOR, ActionType.SKILL, 0, 1, 0, 3)
+            new Skill("Whirlwind", "Spin attack hitting all nearby enemies", EntityClass.WARRIOR, ActionType.SKILL, 0, 3, 5, 4)
         ));
         skillsByClassAndLevel.put(EntityClass.WARRIOR, warriorSkills);
 
@@ -41,15 +39,42 @@ public class SkillsRepository {
             new Skill("Frost Ray", "A ray of frost that slows enemies", EntityClass.WIZARD, ActionType.MAGIC, 1, 1, 3, 1)
         ));
         wizardSkills.put(2, List.of(
-            new Skill("Fireball", "A massive ball of fire", EntityClass.WIZARD, ActionType.MAGIC, 3, 2, 6, 3),
-            new Skill("Arcane Shield", "Protect yourself with arcane energy", EntityClass.WIZARD, ActionType.MAGIC, 2, 0, 0, 2)
+            new Skill("Fireball", "A massive ball of fire", EntityClass.WIZARD, ActionType.MAGIC, 3, 2, 6, 3)
         ));
         wizardSkills.put(3, List.of(
-            new Skill("Lightning Bolt", "A powerful bolt of lightning", EntityClass.WIZARD, ActionType.MAGIC, 3, 3, 6, 4),
-            new Skill("Teleport", "Instantly move to a new location", EntityClass.WIZARD, ActionType.MAGIC, 3, 0, 0, 3)
+            new Skill("Lightning Bolt", "A powerful bolt of lightning", EntityClass.WIZARD, ActionType.MAGIC, 3, 3, 6, 4)
         ));
         skillsByClassAndLevel.put(EntityClass.WIZARD, wizardSkills);
 
+        Map<Integer, List<Skill>> rogueSkills = new HashMap<>();
+        rogueSkills.put(1, List.of(
+            new Skill("Backstab", "A deadly attack from behind", EntityClass.ROGUE, ActionType.SKILL, 0, 10, 3)
+        ));
+        rogueSkills.put(2, List.of(
+            new Skill("Rapid Strike", "Strike Rapidly", EntityClass.ROGUE, ActionType.SKILL, 0, 4, 3, 4)
+        ));
+        skillsByClassAndLevel.put(EntityClass.ROGUE, rogueSkills);
+
+        Map<Integer, List<Skill>> rangerSkills = new HashMap<>();
+        rangerSkills.put(1, List.of(
+            new Skill("Arrow Shot", "A precise arrow attack", EntityClass.RANGER, ActionType.SKILL, 0, 2,4, 6),
+            new Skill("Hunter's Mark", "Mark an enemy to deal extra damage", EntityClass.RANGER, ActionType.SKILL, 0, 8, 0)
+        ));
+        rangerSkills.put(2, List.of(
+            new Skill("Multi-Shot", "Shoot multiple arrows at once", EntityClass.RANGER, ActionType.SKILL, 0, 4, 4, 8)
+        ));
+        skillsByClassAndLevel.put(EntityClass.RANGER, rangerSkills);
+
+        Map<Integer, List<Skill>> clericSkills = new HashMap<>();
+        clericSkills.put(1, List.of(
+            new Skill("Smite", "A divine attack against enemies", EntityClass.CLERIC, ActionType.MAGIC, 2, 1, 8, 6),
+            new Skill("Holy Light", "A beam of Light", EntityClass.CLERIC, ActionType.MAGIC, 2, 1, 5, 8)
+        ));
+        clericSkills.put(2, List.of(
+            new Skill("Holy Blast", "Blasting Spell", EntityClass.CLERIC, ActionType.MAGIC, 0, 1, 8)
+        ));
+        skillsByClassAndLevel.put(EntityClass.CLERIC, clericSkills);
+        
     }
 
     public static List<Skill> getAvailableSkills(EntityClass entityClass, int maxLevel) {
