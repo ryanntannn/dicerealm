d52b2a89ae8ac30f3d5d3a0fd294e0a18580879c
killerdolp <seowdonovan@gmail.com>
2025-04-15 22:54:28 +0800

    Add init combat screen

--- android/app/src/main/java/com/example/dicerealmandroid/util/Initcombat.java
+++ android/app/src/main/java/com/example/dicerealmandroid/util/Initcombat.java
@@ -2,12 +2,16 @@ package com.example.dicerealmandroid.util;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.example.dicerealmandroid.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.textview.MaterialTextView;
@@ -22,23 +26,35 @@ public class Initcombat {
     private FrameLayout loadingLayout;
     private Activity activity;
 
-    public Initcombat(Activity activity){
-        this.message = new MaterialTextView(activity);
-        this.message.setText("Loading...");
-        this.initializeLoadingLayout(activity, message);
-    }
 
     public Initcombat(Activity activity, String message){
         this.message = new MaterialTextView(activity);
-        this.message.setText(message);
-        this.initializeLoadingLayout(activity, this.message);
+        this.initializeLoadingLayout(activity, this.message , message);
     }
 
-    private void initializeLoadingLayout(Activity activity, MaterialTextView message){
+    private void initializeLoadingLayout(Activity activity, MaterialTextView message ,String textmessage){
         this.activity = activity;
         loadingLayout = new FrameLayout(activity);
-        LinearLayout container = new LinearLayout(activity);
+        RelativeLayout container = new RelativeLayout(activity);
+        LinearLayout cardview = new LinearLayout(activity);
         MaterialButton buttonclose = new MaterialButton(activity);
+        TextView textView = new TextView(activity);
+        buttonclose.setText("Start Battle!");
+
+        cardview.setOrientation(LinearLayout.VERTICAL);
+        cardview.setGravity(Gravity.CENTER);
+        cardview.setPadding(40,20,40,20);
+        cardview.setBackgroundResource(R.drawable.card_border);
+
+        textView.setText("Combat time!");
+        textView.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+        textView.setForegroundGravity(Gravity.CENTER);
+
+        View line = new View(activity);
+        line.setBackgroundColor(Color.BLACK);
+        line.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                1));
+
         buttonclose.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -49,7 +65,6 @@ public class Initcombat {
 
         // container prop
         container.setBackgroundColor(Color.TRANSPARENT);
-        container.setOrientation(LinearLayout.VERTICAL);
         container.setGravity(Gravity.CENTER);
 
         // message prop
@@ -57,12 +72,17 @@ public class Initcombat {
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
         );
+        textView.setLayoutParams(textLayout);
+        textView.setPadding(0,0,0, 15);
+        buttonclose.setLayoutParams(textLayout);
         message.setLayoutParams(textLayout);
         message.setForegroundGravity(Gravity.CENTER);
+        message.setTextColor(Color.BLACK);
         message.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Small);
-        message.setTextColor(Color.WHITE);
         message.setTextSize(13);
-        message.setWidth(700);
+        message.setMaxWidth(700);
+        message.setPadding(0,0,0,20);
+        buttonclose.setMaxWidth(700);
 
 
         FrameLayout.LayoutParams progressLayout = new FrameLayout.LayoutParams(
@@ -70,10 +90,12 @@ public class Initcombat {
                 FrameLayout.LayoutParams.MATCH_PARENT
         );
 
-
-        container.addView(message);
-        container.addView(buttonclose);
-
+        Log.d("progressLayout", message.getText().toString());
+        cardview.addView(textView);
+        cardview.addView(message);
+        cardview.addView(buttonclose);
+        container.addView(cardview);
+        displayMessageStream(textmessage,message);
 
         loadingLayout.setBackgroundColor(Color.parseColor("#66000000")); // 40% black
         loadingLayout.setClickable(true);
@@ -101,5 +123,24 @@ public class Initcombat {
                 loadingLayout.setVisibility(View.INVISIBLE);
             });
         }
+    }
+    private void displayMessageStream(String message, TextView currentTurnView) {
+        // Run this on another thread/logical core to achieve true parallelism unlike python which thread is limited by GIL
+        Thread backgroundThread = new Thread(() -> {
+            for (int i = 0; i < message.length(); i++) {
+                char currChar = message.charAt(i);
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                // Important to run on UI thread
+                activity.runOnUiThread(() -> {
+                    currentTurnView.append(String.valueOf(currChar));
+                });
+            }
+        });
+        backgroundThread.start();
     }
 }
